600a98d1547845ec30c77499c1b76c1dda12eace
OdilovMM <dssrinman@gmail.com>
2025-03-17 22:31:13 +0500

    chenging backend address back to another

--- src/utils/backendUrl.jsx
+++ src/utils/backendUrl.jsx
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = "http://localhost:5000/api/v1";
-export const API_BASE_URL = "https://multi-seller-commerce-backend.onrender.com/api/v1";
+// export const API_BASE_URL = "https://multi-seller-commerce-backend.onrender.com/api/v1";
 // export const API_BASE_URL = "https://multi-seller-commerce-backend.vercel.app/api/v1";
+export const API_BASE_URL = "https://multi-seller-commerce-backend-production.up.railway.app/api/v1";
